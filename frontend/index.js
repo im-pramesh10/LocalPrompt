14cfe2ac519a4d35998dbeadc1f27e806c18a93e
Pramesh Mahat <cruisepramesh@gmail.com>
2024-07-20 00:31:44 +0545

    use ollama api

--- frontend/index.js
+++ frontend/index.js
@@ -1,13 +1,14 @@
 async function handleClick() {
     const prompt = document.getElementById('prompt').value
     prompt && console.log(prompt)
-    // const response = await fetch('/api', {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify({prompt: prompt}),
-    // })
-    // const data = await response.json()
-    // document.getElementById('response').textContent = data.response
+    const response = await fetch('http://localhost:8000/prompt', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({prompt: prompt}),
+    })
+    const data = await response.json()
+    console.log(data)
+    document.getElementById('response').textContent = data.message
 }
