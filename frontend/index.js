78d5b8ec6a2f936e8e603aef3b9b82df4ea2c4e0
Pramesh Mahat <cruisepramesh@gmail.com>
2024-07-20 15:15:01 +0545

    error handling

--- frontend/index.js
+++ frontend/index.js
@@ -2,17 +2,26 @@ async function handleClick() {
     const prompt = document.getElementById('prompt').value
     const spinner = document.getElementById('spinner')
     spinner.classList.add('spinner')
-    const response = await fetch('http://localhost:11434/api/generate', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(
-            {"model": "phi", "prompt": prompt, "stream": false}
-        )
-    })
-    const data = await response.json()
-    spinner.classList.remove('spinner')
-    document.getElementById('response').textContent = data.response
-}
+    try {
+        const response = await fetch('http://localhost:11434/api/generate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(
+                {"model": "phi", "prompt": prompt, "stream": false}
+            )
+        })
+        const data = await response.json()
+        if (data.error) {
+            alert(data.error)
+        } else {
+            document.getElementById('response').textContent = data.response
+        }
 
+    } catch (error) {
+        console.log(error)
+        alert(error)
+    }
+    spinner.classList.remove('spinner')
+}
