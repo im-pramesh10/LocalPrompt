513cdb63d811933aae756f28b7f8d2ce094db681
Pramesh Mahat <cruisepramesh@gmail.com>
2024-07-20 22:23:16 +0545

    a new custom function added to modify files

--- backend/api_call.py
+++ backend/api_call.py
@@ -1,12 +1,12 @@
 import aiohttp
 import asyncio
+from settings import OLLAMA_MODEL
 
-
-async def api_call(prompt):
+async def ollama_api_call(prompt):
     async with aiohttp.ClientSession() as session:
         try:
             async with session.post(
-                "http://localhost:11434/api/generate", json={"model": "phi", "prompt": prompt, "stream": False}
+                "http://localhost:11434/api/generate", json={"model": OLLAMA_MODEL, "prompt": prompt, "stream": False}
             ) as response:
                 response_data = await response.json()
                 if response.status != 200:
@@ -17,3 +17,7 @@ async def api_call(prompt):
                 return response_data
         except Exception as e:
             return {'error': str(e)}
+        
+async def custom_model_api(prompt):
+    # define your own custom model here api here
+    return {'response':'You need to set up your custom model function inside api_call.py file inside backend folder'}
