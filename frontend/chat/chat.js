1709965d10f6269f148f454647252b629c3a0be5
Pramesh Mahat <cruisepramesh@gmail.com>
2024-07-22 12:07:35 +0545

    chat api integreted in the frontend

--- frontend/chat/chat.js
+++ frontend/chat/chat.js
@@ -1,18 +1,43 @@
+let conversation_history = [
+    // saved in this format and sent to ollama models to give them context
+    // {
+    // "role": "user",
+    // "content": "why is the sky blue?"
+    // },
+    // {
+    // "role": "assistant",
+    // "content": "due to rayleigh scattering."
+    // },
+    // {
+    // "role": "user",
+    // "content": "how is that different than mie scattering?"
+    // }
+]
+
+let loading = false; // loading state for chat
 function handleEnter(e) {
     if (e.keyCode === 13 && e.shiftKey === false) {
         e.preventDefault()
-        // const prompt = document.getElementById('message')
-        // prompt.blur()
         handleClick()
     }
 }
-function handleClick() {
+async function handleClick() {
     const message = document.getElementById('message').value
+    const spinner = document.getElementById('spinner')
     if (! message) {
         return
     }
+    if (loading) {
+        return
+    }
     addMessage(message, true) // myMessage = true
-    getResponse()
+    conversation_history.push({"role": "user", "content": message})
+    spinner.classList.add('spinner')
+    loading = true
+    const api_response = await getResponse(conversation_history)
+    conversation_history.push({"role": "assistant", "content": api_response})
+    loading = false
+    spinner.classList.remove('spinner')
 }
 
 function addMessage(message, myMessage) {
@@ -21,8 +46,7 @@ function addMessage(message, myMessage) {
     if (myMessage) {
         messageBubble.classList.add('my-message-bubble-container')
         messageBubble.innerHTML = `<p class="message-bubble my-message-bubble">${message}</p>`
-    }
-    else{
+    } else {
         messageBubble.innerHTML = `<p class="message-bubble">${message}</p>`
     }
     const messagesList = document.getElementById('messages-list')
@@ -31,7 +55,28 @@ function addMessage(message, myMessage) {
     document.getElementById('message').focus()
 }
 
-function getResponse() {
-    const message = "WIP feature. Please try again later."
-    addMessage(message, false) // myMessage = false
+async function getResponse(conversation_history) {
+    try {
+        const response = await fetch('http://localhost:8000/chat', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(
+                {"messages": conversation_history}
+            )
+        })
+        const data = await response.json()
+        if (data.error) {
+            alert(data.error)
+            addMessage("Something Went Wrong with ollama model. Make sure it is running and try again", false) // myMessage = false
+        } else {
+            addMessage(data ?. message ?. content, false) // myMessage = false
+            return data ?. message ?. content
+        }
+
+    } catch (error) {
+        alert(error)
+        addMessage("Something Went Wrong with ollama model. Make sure it is running and try again", false) // myMessage = false
+    }
 }
