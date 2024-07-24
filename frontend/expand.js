823f44511bd55f58ed2ba9c2019fc1e2bea34f95
Pramesh Mahat <cruisepramesh@gmail.com>
2024-07-25 04:10:16 +0545

    expand js updated

--- frontend/expand.js
+++ frontend/expand.js
@@ -1,9 +1,20 @@
 function expand() {
     const hiddenElement = document.querySelector('.hidden-radio-select');
+    const hiddenElementContentContainer = document.getElementById('hidden-radio-select-content');
+    const radioSelectContent = document.getElementById('radio-select-content');
+
+    const htmlToSet = radioSelectContent.innerHTML;
+    radioSelectContent.innerHTML = '';
+    hiddenElementContentContainer.innerHTML = htmlToSet;
     hiddenElement.style.display = 'flex';
 }
-function closeModal(e) {
-    e.preventDefault();
+function closeModal(event) {
+    event.preventDefault();
     const hiddenElement = document.querySelector('.hidden-radio-select');
+    const hiddenElementContentContainer = document.getElementById('hidden-radio-select-content');
+    const radioSelectContent = document.getElementById('radio-select-content');
+
+    radioSelectContent.innerHTML = hiddenElementContentContainer.innerHTML;
+    hiddenElementContentContainer.innerHTML = '';
     hiddenElement.style.display = 'none';
 }
