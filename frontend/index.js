8b54a07c0bdf89a95ab922fc02eabceeb2506afb
Pramesh Mahat <cruisepramesh@gmail.com>
2024-07-27 13:26:47 +0545

    state management implemented on header.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,6 @@
 import { expand, closeModal } from "./expand.js";
 import HeaderComponent from "./webComponents/header.js";
 import SinglePrompt from "./webComponents/singlePrompt.js";
-import StateManager from "./stateManager/statemanager.js";
-
-export const globalState = new StateManager({
-    activePage: 'home',
-    modelProvider: 'ollama',
-});
 // Ensure that HeaderComponent is registered
 customElements.define('header-component', HeaderComponent);
 customElements.define('single-prompt', SinglePrompt);
