a549483a9fd1eb2f13ad8d7ba83437622840c0a5
Pramesh Mahat <cruisepramesh@gmail.com>
2024-07-27 20:30:48 +0545

    new globalstate's added

--- frontend/stateManager/globalState.js
+++ frontend/stateManager/globalState.js
@@ -4,4 +4,7 @@ export const globalState = new StateManager({
     activePage: 'home',
     modelProvider: 'ollama',
     systemPrompt: 'You are a helpful assistant',
+    model: 'phi',
+    showAPIKey: false,
+    groqApiKey: localStorage.getItem('groqApiKey') || '',
 });
