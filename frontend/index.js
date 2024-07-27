8134d8e980a5e961d3777638bce017bf73a2a4ad
Pramesh Mahat <cruisepramesh@gmail.com>
2024-07-27 12:24:12 +0545

    simple state management

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,15 @@ import { expand, closeModal } from "./expand.js";
 import HeaderComponent from "./webComponents/header.js";
 import SinglePrompt from "./webComponents/singlePrompt.js";
 
+export let state = {
+    activePage: 'home',
+}
+
+export function setState(stateName, stateValue) {
+    if (sateValue !== state[stateName]) {
+    state[stateName] = stateValue;
+    }
+}
 // Ensure that HeaderComponent is registered
 customElements.define('header-component', HeaderComponent);
 customElements.define('single-prompt', SinglePrompt);
